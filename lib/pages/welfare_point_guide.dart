957a939758e615909a4164db446e1252b8ba3c6c
jytwo <jyl8939@gmail.com>
2024-10-14 21:38:40 +0900

    commit

--- lib/pages/welfare_point_guide.dart
+++ lib/pages/welfare_point_guide.dart
@@ -6,35 +6,35 @@ class WelFarePoint extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 235, 231, 231),
+      backgroundColor: const Color.fromARGB(255, 235, 231, 231),
       appBar: AppBar(
         title: const Text('복지포인트 잔액', style: TextStyle(fontFamily: 'PretendardLight'),),
         centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 235, 231, 231),
+        backgroundColor: const Color.fromARGB(255, 235, 231, 231),
       ),
       body: Center(
         child: Column(
           children: <Widget>[
-            SizedBox(
+            const SizedBox(
               height: 20.0,
               ),
             Container(
-              padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+              padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
               width: 380,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10.0),
-                color: Color.fromARGB(255, 255, 255, 255)
+                color: const Color.fromARGB(255, 255, 255, 255)
               ),
               child: Column(
                 children: <Widget>[
-                  SizedBox(
+                  const SizedBox(
                     height: 10.0,
                   ),  
-                  Text('잔여포인트 : 1,400,000', style: TextStyle(fontFamily: 'PretendardBold',
+                  const Text('잔여포인트 : 1,400,000', style: TextStyle(fontFamily: 'PretendardBold',
                   fontSize: 18.0),
                   textAlign: TextAlign.left,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20.0,
                   ),
                   Container(
@@ -42,97 +42,95 @@ class WelFarePoint extends StatelessWidget {
                     width: 330.0,
                     decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10.0),
-                    color: Color.fromARGB(255, 212, 208, 208)
+                    color: const Color.fromARGB(255, 212, 208, 208)
                     ),
                     child: Container(
                       width: 10,
-                      margin: EdgeInsets.fromLTRB(0.0, 0.0, 40.0, 0.0),
+                      margin: const EdgeInsets.fromLTRB(0.0, 0.0, 40.0, 0.0),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.0),
-                        color: Color.fromARGB(255, 80, 120, 194),
+                        color: const Color.fromARGB(255, 80, 120, 194),
                       ),
                     ),
                   ),
                   Row(children: <Widget>[
-                    SizedBox(
+                    const SizedBox(
                       height: 50.0,
                     ),
                     Container(
                       width: 70.0,
                       height: 10.0,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         shape: BoxShape.circle,
                         color: Color.fromARGB(255, 80, 120, 194),
                       ),
                     ),
-                    Text('사용포인트             3,400,000', style: TextStyle(fontFamily: 'PretendardBold')),
-                    SizedBox(
+                    const Text('사용포인트             3,400,000', style: TextStyle(fontFamily: 'PretendardBold')),
+                    const SizedBox(
                       height: 10.0,
                     ),
                   ],
                   ),
                   Row(children: <Widget>[
-                    SizedBox(
+                    const SizedBox(
                       height: 20.0,
                     ),
                     Container(
                       width: 70.0,
                       height: 10.0,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         shape: BoxShape.circle,
                         color: Color.fromARGB(255, 212, 208, 208),
                       ),
                     ),
-                    Text('총 배정포인트        4,800,000', style: TextStyle(fontFamily: 'PretendardBold')),
+                    const Text('총 배정포인트        4,800,000', style: TextStyle(fontFamily: 'PretendardBold')),
                   ],
                   ),
-                  SizedBox(
+                  const SizedBox(
                       height: 20.0,
                     ),
                 ],
               ),
             ),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 50,
+            Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: 330.0,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    border: Border.all(color: const Color.fromARGB(255, 38, 72, 146), width: 3),
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                  Container(
-                    width: 330.0,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      border: Border.all(color: Color.fromARGB(255, 38, 72, 146), width: 3),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child:  Text('실적 충족 여부 : 충족', style: TextStyle(fontFamily: 'PretendardSemiBold',
-                  fontSize: 15.0),
-                  textAlign: TextAlign.center,
-                  )
+                  child:  const Text('실적 충족 여부 : 충족', style: TextStyle(fontFamily: 'PretendardSemiBold',
+                fontSize: 15.0),
+                textAlign: TextAlign.center,
+                )
+                ),
+                 const SizedBox(
+                  height: 20,
+                ),
+                InkWell(
+                  onTap: () {
+                    debugPrint('조회페이지로 이동');
+                    },
+                    child: Container(
+                  width: 330.0,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 38, 72, 146),
+                    border: Border.all(color: const Color.fromARGB(255, 38, 72, 146), width: 3),
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                   SizedBox(
-                    height: 20,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      print('조회페이지로 이동');
-                      },
-                      child: Container(
-                    width: 330.0,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 38, 72, 146),
-                      border: Border.all(color: Color.fromARGB(255, 38, 72, 146), width: 3),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child:  Text('복지포인트 사용내역 조회', style: TextStyle(fontFamily: 'PretendardSemiBold',
-                  fontSize: 15.0,
-                  color: Color.fromARGB(255, 255, 255, 255)),
-                  textAlign: TextAlign.center,
-                  )
-                  ),
-                  ),
-                ],
-              )
+                  child:  const Text('복지포인트 사용내역 조회', style: TextStyle(fontFamily: 'PretendardSemiBold',
+                fontSize: 15.0,
+                color: Color.fromARGB(255, 255, 255, 255)),
+                textAlign: TextAlign.center,
+                )
+                ),
+                ),
+              ],
             )
           ],
         )
